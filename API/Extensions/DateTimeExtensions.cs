1cd0f02bab8dec33d8bd84cad60acc97fb27c9b2
Jessica <abyjabbylearns@gmail.com>
2022-02-03 20:12:17 -0600

    PostGres & Heroku Deploy

--- API/Extensions/DateTimeExtensions.cs
+++ API/Extensions/DateTimeExtensions.cs
@@ -7,12 +7,13 @@ namespace API.Extensions
 {
     public static class DateTimeExtensions
     {
-        public static int CalculateAge(this DateTime dob){
-            var today = DateTime.Today; 
-            var age = today.Year - dob.Year; 
-            if (dob.Date > today.AddYears(-age)) age--; 
+        public static int CalculateAge(this DateTime dob)
+        {
+            var today = DateTime.UtcNow;
+            var age = today.Year - dob.Year;
+            if (dob.Date > today.AddYears(-age)) age--;
 
-            return age; 
+            return age;
         }
     }
 }
